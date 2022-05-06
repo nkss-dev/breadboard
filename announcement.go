06b9eafc90508a818ec968bf5240a0c1743e2af1
GetPsyched <priyanshutr@gmail.com>
2022-05-06 17:03:54 +0530

    Add CORS header

--- announcement.go
+++ announcement.go
@@ -88,5 +88,6 @@ func GetAnnouncements(w http.ResponseWriter, r *http.Request) {
 
     jsonResp, _ := json.Marshal(announcements)
     w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
     w.Write(jsonResp)
 }
