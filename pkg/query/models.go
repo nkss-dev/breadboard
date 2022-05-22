ab1192a39d8d19403d13b9cc76bd4f2c6e1412d9
Saksham Mittal <gotlouemail@gmail.com>
2022-05-22 13:24:12 +0530

    Add sqlc-generated functions

--- pkg/query/models.go
+++ pkg/query/models.go
@@ -13,13 +13,69 @@ type Course struct {
 	Title      string
 	Branch     string
 	Semester   int16
-	Credits    []int32
+	Credits    []int16
 	Prereq     []string
 	Type       string
 	Objectives string
 	Content    string
 	Books      string
 	Outcomes   string
+}
+
+type Group struct {
+	Name        string
+	Alias       sql.NullString
+	Branch      sql.NullString
+	Kind        sql.NullString
+	Description sql.NullString
+}
+
+type GroupAdmin struct {
+	GroupName  sql.NullString
+	Position   sql.NullString
+	RollNumber sql.NullInt32
+}
+
+type GroupDiscord struct {
+	Name          sql.NullString
+	ID            sql.NullInt64
+	Invite        sql.NullString
+	FresherRole   sql.NullInt64
+	SophomoreRole sql.NullInt64
+	JuniorRole    sql.NullInt64
+	SeniorRole    sql.NullInt64
+	GuestRole     sql.NullInt64
+}
+
+type GroupDiscordUser struct {
+	Batch         int16
+	DiscordUid    sql.NullInt64
+	Name          string
+	Alias         sql.NullString
+	ID            sql.NullInt64
+	Invite        sql.NullString
+	FresherRole   sql.NullInt64
+	SophomoreRole sql.NullInt64
+	JuniorRole    sql.NullInt64
+	SeniorRole    sql.NullInt64
+	GuestRole     sql.NullInt64
+}
+
+type GroupFaculty struct {
+	GroupName string
+	Name      string
+	Mobile    sql.NullInt64
+}
+
+type GroupMember struct {
+	RollNumber int32
+	GroupName  string
+}
+
+type GroupSocial struct {
+	Name sql.NullString
+	Type sql.NullString
+	Link sql.NullString
 }
 
 type Hostel struct {
