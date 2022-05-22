1eacba41c7f380d734cb55007d3eefd5957babc0
Saksham Mittal <gotlouemail@gmail.com>
2022-05-22 13:15:21 +0530

    Update schema.sql with schema of clubs related tables
    This was directly fetched from
    Project-Hyperlink/utils/schemas/groups.sql in commit
    1149902220a82ce4b4d2ad3b37ec76c161e5e246

--- pkg/query/schema.sql
+++ pkg/query/schema.sql
@@ -34,3 +34,59 @@ CREATE TABLE IF NOT EXISTS course (
     outcomes    text NOT NULL,
     PRIMARY KEY (code, branch)
 );
+
+
+CREATE TABLE IF NOT EXISTS groups (
+    name         text       primary key,
+    alias        text       unique,
+    branch       varchar(5) unique,
+    kind         text       check(kind in ('cultural club', 'technical club', 'technical society')),
+    description  text
+);
+
+CREATE TABLE IF NOT EXISTS group_faculty (
+    group_name  text   references groups(name),
+    name        text,
+    mobile      bigint unique,
+    primary key (group_name, name)
+);
+
+CREATE TABLE IF NOT EXISTS group_discord (
+    name            text        references groups(name),
+    id              bigint      unique,
+    invite          varchar(10) unique,
+    fresher_role    bigint      unique,
+    sophomore_role  bigint      unique,
+    junior_role     bigint      unique,
+    senior_role     bigint      unique,
+    guest_role      bigint      unique
+);
+
+CREATE TABLE IF NOT EXISTS group_social (
+    name  text references groups(name),
+    type  varchar(15),
+    link  text
+);
+
+CREATE TABLE IF NOT EXISTS group_admin (
+    group_name   text references groups(name),
+    position     varchar(20),
+    roll_number  int  references student(roll_number)
+);
+
+CREATE TABLE IF NOT EXISTS group_member (
+    roll_number  int  not null references student(roll_number),
+    group_name   text not null references groups(name),
+    primary key(roll_number, group_name)
+);
+
+CREATE OR REPLACE VIEW group_discord_user AS
+    SELECT student.batch, student.discord_uid,
+        c.name, c.alias, d.id, d.invite,
+        d.fresher_role, d.sophomore_role, d.junior_role, d.senior_role,
+        d.guest_role
+        FROM group_member m
+        JOIN groups c ON c.name = m.group_name
+        JOIN group_discord d ON d.name = m.group_name
+        JOIN student ON student.roll_number = m.roll_number
+        WHERE student.discord_uid IS NOT NULL;
