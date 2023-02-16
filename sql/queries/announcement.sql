041cf644a1c9d4c99041e6595a57aac9ed81a17d
Saksham Mittal <gotlouemail@gmail.com>
2023-02-16 12:19:59 +0530

    Remove now-useless announcement insert query

--- sql/queries/announcement.sql
+++ sql/queries/announcement.sql
@@ -1,15 +1,3 @@
--- name: CreateAcademicAnnouncement :exec
-INSERT INTO academic_announcement (
-    date_of_creation, title, title_link, kind
-)
-VALUES (
-    $1,
-    $2,
-    $3,
-    'academic'
-) ON CONFLICT (date_of_creation, title)
-DO NOTHING;
-
 -- name: GetAcademicAnnouncements :many
 SELECT date_of_creation, title, title_link, kind
 FROM academic_announcement
