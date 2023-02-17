e9714995893a9c9593eed67051bf1a94d08cfa7b
GetPsyched <priyanshutr@proton.me>
2023-02-17 17:42:43 +0530

    Add date typecast

--- sql/queries/announcement.sql
+++ sql/queries/announcement.sql
@@ -1,8 +1,16 @@
 -- name: GetLatestAnnouncementDate :one
-SELECT MAX(DISTINCT date_of_creation)
-FROM academic_announcement;
+SELECT
+    MAX(DISTINCT date_of_creation)::DATE
+FROM
+    academic_announcement;
 
 -- name: GetAcademicAnnouncements :many
-SELECT date_of_creation, title, title_link, kind
-FROM academic_announcement
-ORDER BY date_of_creation DESC;
+SELECT
+    date_of_creation,
+    title,
+    title_link,
+    kind
+FROM
+    academic_announcement
+ORDER BY
+    date_of_creation DESC;
