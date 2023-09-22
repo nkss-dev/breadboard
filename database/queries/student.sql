cfc43a071b8a46755372f31226ba068a80c3424b
GetPsyched <priyanshutr@proton.me>
2023-09-22 12:09:17 +0530

    fix: send club data in student query

--- database/queries/student.sql
+++ database/queries/student.sql
@@ -9,10 +9,24 @@ GROUP BY hostel.id;
 
 -- name: GetStudent :one
 SELECT
-    *
+    *,
+    (
+        SELECT
+            COALESCE(JSONB_AGG(JSONB_BUILD_OBJECT(
+                'name', cm.club_name,
+                'alias', club.alias,
+                'position', cm.position,
+                'extra_groups', cm.extra_groups
+            ) ORDER BY cm.club_name), '[]')::JSONB
+        FROM
+            club_member AS cm
+            JOIN club ON club.name = cm.club_name
+        WHERE
+            cm.roll_number = student.roll_number
+    ) AS clubs
 FROM
     student
-WHERE roll_number = $1;
+WHERE student.roll_number = $1;
 
 -- name: GetStudentByDiscordID :one
 SELECT
